a99735c267dfa7dee6177fdf613c1147af2cca4c
Jonathan Streeter <jstreeter@numerex.com>
2015-09-24 12:05:52 -0500

    Removed unessary Files. Updated to 2.25 xc16

---
+++ MM_2015.X/ChipSetup.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ChipSetup.h
+ * Author: Jonathan Streeter
+ *
+ * Created on September 24, 2015, 11:58 AM
+ */
+
+#ifndef CHIPSETUP_H
+#define	CHIPSETUP_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CHIPSETUP_H */
+
