d117add57087632e807a00d74b99e94d87a714c3
Mike <Mike@10.192.1.207>
2015-09-30 15:52:40 -0500

    Added globals.h -added typedefs
    Modify data types in timer setups

---
+++ MM_2015.X/globals.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   globals.h
+ * Author: Mike Peters
+ *
+ * Created on September 30, 2015, 3:43 PM
+ */
+
+#ifndef GLOBALS_H
+#define	GLOBALS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    /*Unsigned typedefs*/
+    typedef unsigned char       U8;
+    typedef unsigned short int  U16;
+    typedef unsigned long       U32;
+    typedef unsigned long long  U64;
+    
+    /*Signed typedefs*/
+    typedef signed char         S8;
+    typedef signed short int    S16;
+    typedef signed long         S32;
+    typedef signed long long    S64;
+    
+    
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GLOBALS_H */
+
